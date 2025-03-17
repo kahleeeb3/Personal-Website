363a87052004f456b2d1e76e0319d591d912a771
kahleeeb3 <powellcalebm@gmail.com>
2025-03-16 21:03:37 -0400

    Added Collapsible Divs

---
+++ src/js/collapsible.js
@@ -0,0 +1,34 @@
+function toggleCollapsible(event){
+    const button = event.target;
+    const div = button.nextElementSibling;
+
+    if(div.classList.contains("expanded")){
+        div.style.maxHeight = "0px";
+        button.textContent = div.dataset.title + "\u23F5";
+    }
+    else{
+        div.style.maxHeight = "";
+        button.textContent = div.dataset.title + "\u23F7";
+    }
+    div.classList.toggle("expanded");
+}
+
+function createCollapsible(){
+    const div = document.getElementById("collapsible");
+    const button = document.createElement('button');
+
+    if(div.classList.contains("expanded")){
+        button.textContent = div.dataset.title + "\u23F7";
+    }
+    else{
+        div.style.maxHeight = "0px";
+        button.textContent = div.dataset.title + "\u23F5";
+    }
+    
+    button.classList.add("collapsibleButton");
+    button.addEventListener("click", toggleCollapsible);
+    div.parentNode.insertBefore(button, div);
+}
+
+
+createCollapsible();
