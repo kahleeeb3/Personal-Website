43d80cd08ca4a86fb5e3fbb27f4347486110ccc8
kahleeeb3 <powellcalebm@gmail.com>
2025-03-21 16:09:45 -0400

    Added Subpage Feature

--- src/js/loadContent.js
+++ src/js/loadContent.js
@@ -1,6 +1,17 @@
-var currSidebar = null; // current sidebar displayed
-var currPage = null; // current page loaded
-var page = null; // page the user is trying to reach
+currPage = null;
+currSidebar = null;
+
+function loadJSON(file, callback){
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to load ${file}`);
+            }
+            return response.json();
+        })
+        .then(data => callback(data))
+        .catch(error => console.error("Error:", error));
+}
 
 function readFile(file, callback) {
     fetch(file)
@@ -12,6 +23,17 @@ function readFile(file, callback) {
         })
         .then(data => callback(data))
         .catch(error => console.error("Error:", error));
+}
+
+function insertHTML(element, path, callback){
+    const HTMLElement = document.getElementById(element);
+
+    readFile(path, function(newHTML){
+        HTMLElement.innerHTML = newHTML;
+        if (callback) {
+            callback();
+        }
+    });
 }
 
 function insertStyles(element, path) {
@@ -30,42 +52,29 @@ function insertScripts(element, path){
     HTMLElement.appendChild(script);
 }
 
-function insertHTML(element, path, callback){
-    const HTMLElement = document.getElementById(element);
-
-    readFile(path, function(newHTML){
-        HTMLElement.innerHTML = newHTML;
-        currPage = page;
-        if (callback) {
-            callback();
-        }
-    });
-}
-
 function parseJSON(data){
-    // if page is already loaded, do nothing
-    if(page == currPage) return;
 
     // get the page we need
-    if (data.homePage != null) {
-        insertHTML("content", `/pages/${page}/${data.homePage}`, function(){
-            currPage = page; // set new homepage
+    if(data.page != null){
+        insertHTML("content", `/pages/${data.page}/${data.content}`, function(){
+            currPage = data.page; // set the new page
             requestAnimationFrame(() => Prism.highlightAll()); // allow syntax highlight
             createCollapsible(); // allow collapsible
 
             // get any scripts we need
             if (data.scripts != null) {
-                insertScripts("content", `/pages/${page}/src/js/${data.scripts}`);
+                insertScripts("content", `/pages/${data.page}/src/js/${data.scripts}`);
             }
 
             // get any styles we need
             if (data.styles != null){
-                insertStyles("content", `/pages/${page}/src/css/${data.styles}`);
+                insertStyles("content", `/pages/${data.page}/src/css/${data.styles}`);
             }
+
         });
     }
 
-    // Get correct sidebar
+    // get the sidebar we need
     if(data.sidebar == null){
         if(currSidebar != "home"){
             insertHTML("sidebar", "/pages/home/sidebar.html");
@@ -74,52 +83,42 @@ function parseJSON(data){
         }
     }
     else{
-        if(currSidebar != page) {
-            insertHTML("sidebar", `/pages/${page}/${data.sidebar}`);
-            currSidebar = page;
+        if(currSidebar != data.page) {
+            insertHTML("sidebar", `/pages/${data.page}/${data.sidebar}`);
+            currSidebar = data.page;
             // console.log(`Loading ${page} Sidebar`);
         }
     }
 }
 
-function loadJSON(file, callback){
-    fetch(file)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to load ${file}`);
-            }
-            return response.json();
-        })
-        .then(data => callback(data))
-        .catch(error => console.error("Error:", error));
-}
+function getDesiredPage(urlParams){
 
-function getCurrPageName() {
-    const queryString = window.location.search; // get current page url
-    const urlParams = new URLSearchParams(queryString);
-    const pageParam = urlParams.get('page'); // get the page param
-    if (pageParam == null || page == ""){
-        return "home";
-    }
-    return pageParam;
+    // get parameters from the url
+    const pageParam = urlParams.get('page') ?? "home";
+    const subPageParam = urlParams.get('subpage');
+
+    // load desired content
+    loadJSON(`/pages/${pageParam}/src/json/page.json`, function(data){
+        if(data.content[subPageParam-1] != null) {
+            data.content = data.content[subPageParam-1];
+        }
+        else{
+            data.content = data.content[0];
+        }
+        parseJSON(data);
+        document.title = pageParam.replace(/\b\w/g, char => char.toUpperCase());
+    });
+
 }
 
 // listen for page loads
 document.addEventListener("DOMContentLoaded", function () {
-    // get the desired url
-    page = getCurrPageName();
-    // load desired page
-    loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
-    document.title = page.replace(/\b\w/g, char => char.toUpperCase()); // capitalize the first letter
-});
+    // get url params from browser search bar
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
 
-// listen for user hitting back button
-window.addEventListener('popstate', function(event){
-    // get the desired url
-    page = getCurrPageName();
     // load desired page
-    loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
-    document.title = page.replace(/\b\w/g, char => char.toUpperCase());
+    getDesiredPage(urlParams);    
 });
 
 // listen for clicking internal links
@@ -128,18 +127,23 @@ document.addEventListener('click', function(event) {
         // prevent default behavior
         event.preventDefault();
 
-        // get the desired page
+        // get url params from internal link
         const url = new URL(event.target.href);
-        page = url.searchParams.get('page');
+        const urlParams = url.searchParams;
 
-        // if no page, set to home page
-        if (page == null || page == ""){
-            page = "home"
-        }
+        getDesiredPage(urlParams);
 
-        // load desired page and push browser history
-        loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
-        history.pushState(null, page, event.target.href);
-        document.title = page.replace(/\b\w/g, char => char.toUpperCase()); // capitalize the first letter
+        // update history
+        history.pushState(null, null, event.target.href);
     }
+});
+
+// listen for user hitting back button
+window.addEventListener('popstate', function(event){
+    // get url params from browser search bar
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    // load desired page
+    getDesiredPage(urlParams);
 });
