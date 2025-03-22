25561cd72b6e7e50e0f9ece7fa458d18add4fe3c
Caleb Powell <62320535+kahleeeb3@users.noreply.github.com>
2025-03-21 21:30:38 -0400

    Revert "Handling Internal links working"
    This reverts commit ea3dbbfd888f341297d4adf631ac649892becfe1.

--- README.md
+++ README.md
@@ -6,9 +6,3 @@
 ```
 python3 -m http.server
 ```
-
-# To Do:
-- Am I inserting scripts more than once on page load?
-- Add collapsible sections
-- Need to be able to link thread posts to subsections of larger pages
-- Add version to scripts so the scripts are forced to reload every time.
--- src/js/loadContent.js
+++ src/js/loadContent.js
@@ -5,11 +5,11 @@ var page = null; // page the user is trying to reach
 function getCurrPageName() {
     const queryString = window.location.search; // get current page url
     const urlParams = new URLSearchParams(queryString);
-    const pageParam = urlParams.get('page'); // get the page param
-    if (pageParam == null || page == ""){
-        return "home";
+    const page = urlParams.get('page'); // get the page param
+    if (page == null || page == ""){
+        return "home"
     }
-    return pageParam;
+    return page
 }
 
 function loadHTML(file, callback) {
@@ -44,7 +44,6 @@ function insertStyles(file){
 }
 
 function insertScripts(element, file){
-    // Note: wont this keep appending multiple scripts? Should look into that.
     const HTMLElement = document.getElementById(element);
     const script = document.createElement("script");
     script.src = `/pages/${page}/src/js/${file}`;
@@ -53,6 +52,9 @@ function insertScripts(element, file){
 }
 
 function insertSidebar(element, file){
+
+    // what about if the homebar is already loaded and we visit home?
+
     // does page have a dedicated sidebar
     if(file != null){
         // is that sidebar already loaded?
@@ -103,19 +105,14 @@ function insertHTML(element, file){
         if(currPage != page){
             HTMLElement.innerHTML = newHTML;
             currPage = page;
+            // history.pushState({ page: page }, "", `/?page=${page}`);
             requestAnimationFrame(() => Prism.highlightAll()); // ensure prism runs when new content loads
         }
     });
 }
 
 function parseJSON(data){
-    if(page == currPage){
-        return; // don't load pages twice
-    }
-
-    // load the sidebar
     insertSidebar("sidebar", data.sidebar);
-
     if (data.styles != null) {
         insertStyles(data.styles);
     }
@@ -127,40 +124,14 @@ function parseJSON(data){
     }
 }
 
-// listen for page loads
 document.addEventListener("DOMContentLoaded", function () {
-    // get the desired url
     page = getCurrPageName();
-    // load desired page
     loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
 });
 
-// listen for user hitting back button
-window.addEventListener('popstate', function(event){
-    // get the desired url
-    page = getCurrPageName();
-    // load desired page
-    loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
-});
-
-// listen for clicking internal links
 document.addEventListener('click', function(event) { 
     if (event.target.classList.contains('internal-link') && event.target.hasAttribute('href')) {
-        // prevent default behavior
         event.preventDefault();
-
-        // get the desired page
-        const url = new URL(event.target.href);
-        page = url.searchParams.get('page');
-
-        // if no page, set to home page
-        if (page == null || page == ""){
-            page = "home"
-        }
-
-        // load desired page and push browser history
-        loadJSON(`/pages/${page}/src/json/page.json`, parseJSON);
-        history.pushState(null, page, event.target.href);
-        document.title = page.replace(/\b\w/g, char => char.toUpperCase()); // capitalize the first letter
+        console.log("internal links disabled");
     }
 });
